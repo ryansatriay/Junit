8f653974c2f64adeb477e6a2198c5e97c49c89bd
ryanyudhistira <ryan.yudhistira@accenture.com>
2022-10-18 13:03:21 +0700

    Add

---
+++ src/main/java/com/example/demo/Math.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Math {
+    public double add (int a, int b) {
+        return a + b;
+    }
+
+    public double min (int a, int b) {
+        return a - b;
+    }
+
+    public double mul (int a, int b) {
+        return a * b;
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	Math m = new Math();
+
 	@Test
-	void contextLoads() {
+	public void addTest() {
+		double expected = 4d;
+		double actual = m.add(2,2);
+		Assert.assertTrue(expected == actual);
+	}
+
+	@Test
+	public void minTest() {
+		double expected = 2d;
+		double actual = m.add(4,2);
+		Assert.assertTrue(expected == actual);
+	}
+
+	@Test
+	public void mulTest() {
+		double expected = 8d;
+		double actual = m.add(4,2);
+		Assert.assertTrue(expected == actual);
 	}
 
 }
